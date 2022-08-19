450b6daf647b75e77e475c609133d971a7ab1aed
tristayqc <qinchen.yao@vanderbilt.edu>
2022-08-19 18:33:28 -0500

    change color & style

--- concatPlot.py
+++ concatPlot.py
@@ -3,6 +3,8 @@ from codecs import ignore_errors
 import pandas as pd
 import os
 import glob
+import matplotlib.pyplot as plt
+import matplotlib.patches as PathPatch
 
 pathFS = r'C:\Users\Trist\Documents\GitHub\BoxPlot\ForestS'
 pathFNS = r'C:\Users\Trist\Documents\GitHub\BoxPlot\ForestNS'
@@ -35,15 +37,26 @@ df_fns.columns = ['Forest_NS']
 df_S.columns = ['Salient']
 df_NS.columns = ['Non-salient']
 
-# # df of all four conditions
-# df_all4 = pd.concat([df_us, df_uns, df_fs, df_fns], axis = 1)
-# df_all4.boxplot(figsize = (5, 5))
+plt.figure(figsize=(5, 8))
 
-# # df of 2 saliency condition
-df_bothS = pd.concat([df_S, df_NS], axis = 1)
-df_bothS.boxplot(figsize = (5, 5))
+# df of all four conditions
+df_all4 = pd.concat([df_us, df_uns, df_fs, df_fns], axis = 1)
+box_4 = plt.boxplot(df_all4, vert=True, labels = ['Urban_S', 'Urban_NS', 'Forest_S', 'Forest_NS'], patch_artist=True, showfliers=False, widths=(0.25, 0.25, 0.25, 0.25))
+
+# df of 2 saliency condition
+# df_bothS = pd.concat([df_S, df_NS], axis = 1)
+# box_SNS = plt.boxplot(df_bothS, vert = True, labels = ['Salient', 'Non-salient'], patch_artist = True, showfliers = False, widths=(0.25, 0.25))
+
+# fill with colors
+# colorsSNS = ['#FED8B1', '#E5E4E2']
+# for patch, color in zip(box_SNS['boxes'], colorsSNS):
+#     patch.set_facecolor(color)
+colors4 = ['#50EBEC', '#CCFFFF', '#6CC417', '#DBF9DB']
+for patch, color in zip(box_4['boxes'], colors4):
+    patch.set_facecolor(color)
+
+plt.show()
+
 
 # pd.set_option("display.max_rows", None, "display.max_columns", None)
 # print(df_S)
-
-# %%
